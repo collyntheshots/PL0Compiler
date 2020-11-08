178b87536b74ce9bd7914d2285765b02cc600bcd
Collyn <clindley@knights.ucf.edu>
2020-11-07 21:24:32 -0500

    Project Skeleton

---
+++ lex.c
@@ -0,0 +1,11 @@
+
+#include "lex.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+lexeme* lex_analyze(char *inputfile)
+{
+	lexeme *list = malloc(500 * sizeof(lexeme));
+	printf("lex is analyzed\n");
+	return list;
+}

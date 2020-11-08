538d684131f510fa490e04ce7a071a1848d70c22
Collyn <clindley@knights.ucf.edu>
2020-11-08 12:59:03 -0500

    minor edits

--- lex.h
+++ lex.h
@@ -18,7 +18,7 @@ typedef enum {
 // token type
 typedef enum {
 nulsym = 1, identsym, numbersym, plussym, minussym,
-multsym,  slashsym, oddsym, eqsym, neqsym, lessym, leqsym,
+multsym, slashsym, oddsym, eqsym, neqsym, lessym, leqsym,
 gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
 periodsym, becomessym, beginsym, endsym, ifsym, thensym,
 whilesym, dosym, callsym, constsym, varsym, procsym, writesym,

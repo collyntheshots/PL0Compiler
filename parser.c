d07a7e3a5974bc88f732211a583aed1c80629f27
Collyn <clindley@knights.ucf.edu>
2020-11-08 01:46:35 -0500

    basic outline

--- parser.c
+++ parser.c
@@ -3,9 +3,18 @@
 #include "parser.h"
 #include "lex.h"
 
+
+
 symbol* parse(lexeme *list)
 {
 	symbol *table = malloc(500 * sizeof(symbol));
 	printf("symbols are parsed\n");
 	return table;
+}
+
+int main(int argc, char **argv)
+{
+	lexeme *list = lex_analyze(argv[1]);
+	parse(list);
+	return 0;
 }
